cad41ce3d1e76699613be5a4f7b0517cdef55c63
amit.prakash.singh <amit.prakash.singh@accenture.com>
2016-10-19 16:54:08 +0100

    a bit intelligent bot

--- server.js
+++ server.js
@@ -1,10 +1,64 @@
 var fs = require('fs'),
-    path = require('path'),
-    Twit = require('twit'),
-    config = require(path.join(__dirname, 'config.js'));
+path = require('path'),
+Twit = require('twit'),
+config = require(path.join(__dirname, 'config.js'));
 
 var T = new Twit(config);
 
-T.post('statuses/update', { status: 'How @arvindkejriwal is changing face of education in delhi https://goo.gl/n6dvtm' }, function(err, data, response) {
-  console.log(data)
-});
+function postTweet(tweetText) {
+
+	T.post('statuses/update', {
+		status : tweetText
+	}, function (err, data, response) {
+		console.log(data);
+	});
+
+}
+
+function showQuery() {
+
+	T.get('search/tweets', {
+		q : '@narendramodi',
+		count : 5
+	}, function (err, data, response) {
+
+		var mentionArray = [];
+		statuses = data.statuses
+
+			if (statuses) {
+				for (i = 0; i < statuses.length; i++) {
+					jsonStatusData = statuses[i]
+
+						if (jsonStatusData.text.toUpperCase().indexOf("SURGICAL")) {
+							console.log(jsonStatusData.text);
+
+							var result = jsonStatusData.text.split(" ");
+							var k = 0;
+							for (j = 0; j < result.length; j++) {
+
+								if (result[j].charAt(0) == '@') {
+									console.log('Mentions = ' + result[j]);
+									mentionArray[k] = result[j];
+									k = k + 1;
+
+								}
+
+							}
+							
+							var tweetText = 'Don\'t play with soldier\'s life '
+							for(l = 0; l <mentionArray.length ; l++)
+							{ tweetText += mentionArray[l] + " " 
+							
+								}
+							
+							postTweet(tweetText)
+						}
+
+				}
+			}
+
+	});
+
+}
+
+setInterval(showQuery, 5000);
